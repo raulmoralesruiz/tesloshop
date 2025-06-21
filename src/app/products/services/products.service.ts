31f04ce5932cd6456f1f26153bb089989d5722c9
Raúl <raulmdaw@gmail.com>
2025-06-21 21:06:28 +0200

    Pantalla de producto

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { ProductsResponse } from '@products/interfaces/product.interface';
+import {
+  Product,
+  ProductsResponse,
+} from '@products/interfaces/product.interface';
 import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -18,11 +21,16 @@ export class ProductsService {
 
   getProducts(options: Options): Observable<ProductsResponse> {
     const { limit = 9, offset = 0, gender = '' } = options;
+    const url = `${baseUrl}/products`;
 
-    return this.http
-      .get<ProductsResponse>(`${baseUrl}/products`, {
-        params: { limit, offset, gender },
-      })
-      .pipe(tap((resp) => console.log(resp)));
+    return this.http.get<ProductsResponse>(url, {
+      params: { limit, offset, gender },
+    });
+    // .pipe(tap((resp) => console.log(resp)));
+  }
+
+  getProductByIdSlug(idSlug: string): Observable<Product> {
+    const url = `${baseUrl}/products/${idSlug}`;
+    return this.http.get<Product>(url); //.pipe(tap((resp) => console.log(resp)));
   }
 }
--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, linkedSignal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
+import { ProductsService } from '../../../products/services/products.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-page',
   imports: [],
   templateUrl: './product-page.component.html',
 })
-export class ProductPageComponent { }
+export class ProductPageComponent {
+  productsService = inject(ProductsService);
+  activatedRoute = inject(ActivatedRoute);
+
+  // productIdSlug = id de la ruta activa
+  productIdSlugParam = this.activatedRoute.snapshot.params['idSlug'];
+  productIdSlug = linkedSignal(() => this.productIdSlugParam);
+
+  // rxResource
+  productResource = rxResource({
+    request: () => ({ idSlug: this.productIdSlug() }),
+    loader: ({ request }) => {
+      // if( !request.idSlug ) return of([]); // of devuelve un obersvable, con un array vacío
+
+      return this.productsService.getProductByIdSlug(this.productIdSlug());
+    },
+  });
+}
