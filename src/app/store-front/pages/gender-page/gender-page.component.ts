19b96d8ecca4e97b846401da732f4416adc98154
Raúl <raulmdaw@gmail.com>
2025-06-22 17:53:02 +0200

    Servicio de paginación

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -4,22 +4,31 @@ import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
 import { ProductsService } from '@products/services/products.service';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
+import { PaginationComponent } from '@shared/components/pagination/pagination.component';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './gender-page.component.html',
 })
 export class GenderPageComponent {
   route = inject(ActivatedRoute);
   productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
 
   gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
 
   productsResource = rxResource({
-    request: () => ({ gender: this.gender() }),
+    request: () => ({
+      gender: this.gender(),
+      page: this.paginationService.currentPage() - 1,
+    }),
     loader: ({ request }) => {
-      return this.productsService.getProducts({ gender: request.gender });
+      return this.productsService.getProducts({
+        gender: request.gender,
+        offset: request.page * 9,
+      });
     },
   });
 }
