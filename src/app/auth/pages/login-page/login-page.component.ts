604737b3dc3556e93cec4a7f85e634216c13698f
Raúl <raulmdaw@gmail.com>
2025-06-27 20:09:19 +0200

    Manejo de excepciones

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '@auth/services/auth.service';
 
@@ -12,6 +12,7 @@ export class LoginPageComponent {
   fb = inject(FormBuilder);
   hasError = signal(false);
   isPosting = signal(false);
+  router = inject(Router);
 
   authService = inject(AuthService);
 
@@ -30,8 +31,13 @@ export class LoginPageComponent {
     }
 
     const { email = '', password = '' } = this.loginForm.value;
-    this.authService.login(email!, password!).subscribe((resp) => {
-      console.log(resp);
+    this.authService.login(email!, password!).subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.router.navigateByUrl('/');
+        return;
+      }
+
+      this.hasError.set(true);
     });
   }
 }
