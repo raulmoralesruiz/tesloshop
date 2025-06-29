5bf6e5945a7fb11656e72e71220135a86cc22e41
Raúl <raulmdaw@gmail.com>
2025-06-29 17:26:07 +0200

    Listado de productos

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductTableComponent } from '@products/components/product-table/product-table.component';
+import { ProductsService } from '@products/services/products.service';
+import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [],
+  imports: [ProductTableComponent, PaginationComponent],
   templateUrl: './products-admin-page.component.html',
 })
-export class ProductsAdminPageComponent { }
+export class ProductsAdminPageComponent {
+  productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
+  productsResource = rxResource({
+    request: () => ({ page: this.paginationService.currentPage() - 1 }),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({
+        offset: request.page * 9,
+      });
+    },
+  });
+}
